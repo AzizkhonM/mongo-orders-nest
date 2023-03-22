46d02c6dc70b1e69940b71724e8c6d5e3eee757b
Azizxon <muzaffarovazizkhon@gmail.com>
2023-03-22 18:29:09 +0500

    Added Order, Operation and Status

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,7 @@
-export class CreateOrderDto {}
+export class CreateOrderDto {
+    full_name: string
+    phone_number: string
+    product_link: string
+    sum: number
+    email: string
+}
--- src/order/schemas/order.schema.ts
+++ src/order/schemas/order.schema.ts
@@ -1,1 +1,37 @@
-export class Order {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { CurrencyType } from '../../currency_type/schemas/currency_type.schema';
+
+export type OrderDocument = HydratedDocument<Order>;
+
+@Schema()
+export class Order {
+  @Prop()
+  order_unique_id: string
+
+  @Prop()
+  full_name: string;
+
+  @Prop()
+  phone_number: string;
+
+  @Prop()
+  product_link: string;
+
+  @Prop()
+  sum: number;
+
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: "CurrencyType"})
+  currency_type_id: CurrencyType;
+
+  @Prop()
+  truck: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  description: string;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
